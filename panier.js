6969101b346c89dd457cb9312541790a7125658c
glow secrets <raslenbch1717@gmail.com>
2026-07-08 18:15:45 +0100

    Update panier.js

--- panier.js
+++ panier.js
@@ -1,54 +1,23 @@
-function ajouterPanier(nom, prix, image){
+let zonePanier = document.getElementById("zonePanier");
 
-    let panier = JSON.parse(localStorage.getItem("panier")) || [];
+let panier = JSON.parse(localStorage.getItem("panier")) || [];
 
-    panier.push({
-        nom: nom,
-        prix: prix,
-        image: image
-    });
+if (panier.length === 0) {
 
-    localStorage.setItem("panier", JSON.stringify(panier));
+    zonePanier.innerHTML = "<p>Votre panier est vide</p>";
 
-    alert("Ajouté au panier ✅");
-}
+} else {
 
+    panier.forEach(produit => {
 
-
-let zonePanier = document.getElementById("panier");
-
-
-if(zonePanier){
-
-    let panier = JSON.parse(localStorage.getItem("panier")) || [];
-
-
-    if(panier.length === 0){
-
-        zonePanier.innerHTML = "Votre panier est vide 🛒";
-
-    } else {
-
-        zonePanier.innerHTML = "";
-
-        panier.forEach(produit => {
-
-            zonePanier.innerHTML += `
-
-            <div class="carte">
-
-                <img src="${produit.image}" width="150">
-
+        zonePanier.innerHTML += `
+            <div class="produit">
+                <img src="${produit.image}" width="100">
                 <h3>${produit.nom}</h3>
-
                 <p>${produit.prix} DT</p>
-
             </div>
+        `;
 
-            `;
-
-        });
-
-    }
+    });
 
 }
